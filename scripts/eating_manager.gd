b12ab5068d5e225dd02193aff33519e6375c92d8
Erica-99 <turbocake17@gmail.com>
2026-02-21 21:10:53 +1100

    Consumed resourced added to inventory

--- scripts/eating_manager.gd
+++ scripts/eating_manager.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
-
 @export var consumption_rate: float = 100 # In item/sec
 
 var actor: RigidBody2D
 var time_passed_since_eating: float = 0
+var inventory: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	actor = get_parent()
+	inventory = $Inventory
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,16 +23,32 @@ func _process(delta: float) -> void:
 			print(actor.landing_target.remaining_resource_quantity)
 			if actor.landing_target.carbon > 0:
 				actor.landing_target.carbon -= int(stacked_time)
+				if actor.landing_target.carbon < 0:
+					stacked_time = stacked_time + actor.landing_target.carbon
+					actor.landing_target.carbon = 0
 				actor.landing_target.remaining_resource_quantity -= int(stacked_time)
+				inventory.carbon_stores += int(stacked_time)
 			elif actor.landing_target.biomass > 0:
 				actor.landing_target.biomass -= int(stacked_time)
+				if actor.landing_target.biomass < 0:
+					stacked_time = stacked_time + actor.landing_target.biomass
+					actor.landing_target.biomass = 0
 				actor.landing_target.remaining_resource_quantity -= int(stacked_time)
+				inventory.biomass_stores += int(stacked_time)
 			elif actor.landing_target.minerals > 0:
 				actor.landing_target.minerals -= int(stacked_time)
+				if actor.landing_target.minerals < 0:
+					stacked_time = stacked_time + actor.landing_target.minerals
+					actor.landing_target.minerals = 0
 				actor.landing_target.remaining_resource_quantity -= int(stacked_time)
+				inventory.mineral_stores += int(stacked_time)
 			elif actor.landing_target.nanotech > 0:
 				actor.landing_target.nanotech -= int(stacked_time)
+				if actor.landing_target.nanotech < 0:
+					stacked_time = stacked_time + actor.landing_target.nanotech
+					actor.landing_target.nanotech = 0
 				actor.landing_target.remaining_resource_quantity -= int(stacked_time)
+				inventory.nanotech_stores += int(stacked_time)
 			else:
 				print("planet killed")
 				reset_eating()
---
+++ scripts/inventory.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@export var carbon_stores: int = 0
+@export var biomass_stores: int = 0
+@export var mineral_stores: int = 0
+@export var nanotech_stores: int = 0
