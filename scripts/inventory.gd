720a68b9ba434dd406fa3bdbee7b0cce26db7b01
Erica-99 <turbocake17@gmail.com>
2026-02-22 16:04:53 +1100

    Limit carbon stores

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-@export var carbon_stores: int = 0
+@export var max_carbon: int = 100
+
+@export var carbon_stores: int = 0:
+	set(value):
+		carbon_stores = clampi(value, 0, max_carbon)
 @export var biomass_stores: int = 0
 @export var mineral_stores: int = 0
 @export var nanotech_stores: int = 0
