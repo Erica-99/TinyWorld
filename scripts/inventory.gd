61a66805d8e00506effcdafd24a6007f262b9b66
Erica-99 <turbocake17@gmail.com>
2026-02-24 23:19:26 +1100

    Fixed surge ui and surge stocks in inventory

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
 @export var max_carbon: int = 500
-@export var max_mineral: int = 500
+@export var max_surges: int = 5
+@export var minerals_per_surge: int = 100
 
-@export var max_surges: int = 4
-@export var ready_surges: int = 1
-@export var carbon_stores: int = 50:
+@export var carbon_stores: int = 0:
 	set(value):
 		carbon_stores = clampi(value, 0, max_carbon)
-@export var biomass_stores: int = 50
+@export var biomass_stores: int = 0
 @export var mineral_stores: int = 0:
 	set(value):
-		mineral_stores = clampi(value, 0, max_mineral)
+		mineral_stores = clampi(value, 0, max_surges * minerals_per_surge)
 @export var nanotech_stores: int = 0
+
+var ready_surges: int:
+	get():
+		return int(mineral_stores/minerals_per_surge)
+
+
+func use_surge() -> void:
+	mineral_stores -= minerals_per_surge
+	mineral_stores = clampi(mineral_stores, 0, max_surges * minerals_per_surge)
