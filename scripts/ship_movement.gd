dc78ac6a6558572dae8b7a47d24291df1307891e
Erica-99 <turbocake17@gmail.com>
2026-02-25 17:05:32 +1100

    Implement surge movement

--- scripts/ship_movement.gd
+++ scripts/ship_movement.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var turning_torque: float = 10
 @export var linear_damper_strength: float = 5
 @export var angular_damper_strength: float = 5
+@export var surge_strength: float = 2000
 
 var ready_to_land: bool = false
 
@@ -17,26 +18,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# Check if there are any nearby planets
-	if actor.nearby_planets.size() > 0:
-		# Check if conditions met to ready landing
-		var nearest_planet = actor.get_nearest_planet()
-		if (actor.get_distance_to_planet_surface(nearest_planet) < actor.max_range_for_landing
-		and actor.linear_velocity.length() < actor.max_velocity_for_landing):
-			actor.set_ready_to_land()
+	if surge_status == ENUMS.SurgeStatus.NONE:
+		# Check if there are any nearby planets
+		if actor.nearby_planets.size() > 0:
+			# Check if conditions met to ready landing
+			var nearest_planet = actor.get_nearest_planet()
+			if (actor.get_distance_to_planet_surface(nearest_planet) < actor.max_range_for_landing
+			and actor.linear_velocity.length() < actor.max_velocity_for_landing):
+				actor.set_ready_to_land()
+			else:
+				actor.enter_default_mode()
 		else:
 			actor.enter_default_mode()
-	else:
-		actor.enter_default_mode()
 
 
 func _physics_process(delta: float) -> void:
-	match actor.movement_mode:
-		ENUMS.PlayerMovementMode.DEFAULT:
+	match surge_status:
+		ENUMS.SurgeStatus.NONE:
 			process_movement(delta)
-		ENUMS.PlayerMovementMode.READY_TO_LAND:
-			process_movement(delta)
-			
+		ENUMS.SurgeStatus.AIMING:
+			aim_surge()
+		ENUMS.SurgeStatus.FLYING:
+			cruise_surge()
 
 
 ## Processes input and moves the ship in normal mode
@@ -54,25 +57,44 @@ func process_movement(delta: float) -> void:
 
 
 func aim_surge() -> void:
-	actor.linear_damp = 10
-	actor.angular_damp = 1
-	
 	var mouse_pos = get_global_mouse_position()
 	var target_angle = actor.global_position.angle_to_point(mouse_pos)
-	var angle_diff = wrapf(target_angle - actor.rotation - PI/2, -PI, PI)
-	actor.apply_torque(angle_diff * 5000)
-	pass
+	var angle_diff = wrapf(target_angle - actor.rotation + PI/2, -PI, PI)
+	actor.apply_torque(angle_diff * 200000)
 
 func fire_surge() -> void:
 	# Code to fire impulse
+	actor.linear_damp = 2
+	actor.angular_damp = 10
+	
+	var surge_direction_vector = (get_global_mouse_position() - actor.global_position).normalized()
+	actor.apply_impulse(surge_direction_vector * surge_strength, Vector2.ZERO)
 	
 	actor.inventory.use_surge()
-	pass
+	surge_status = ENUMS.SurgeStatus.FLYING
 
 func cruise_surge() -> void:
 	# Lock up input for a second or so after surging or until you hit something
+	if actor.linear_velocity.length() < 300:
+		return_to_no_surge()
+
+
+func set_surge_aiming() -> void:
+	actor.linear_damp = 10
+	actor.angular_damp = 10
 	
-	pass
+	actor.sprites.play("Idle")
+	
+	surge_status = ENUMS.SurgeStatus.AIMING
+
+
+func return_to_no_surge() -> void:
+	actor.linear_damp = 0
+	actor.angular_damp = 2
+	
+	actor.sprites.play("Flying")
+	
+	surge_status = ENUMS.SurgeStatus.NONE
 
 
 ## One-off inputs (dampers, landing mode)
@@ -89,9 +111,8 @@ func _unhandled_input(event):
 	
 	if event.is_action_pressed("Surge"):
 		if actor.inventory.ready_surges:
-			surge_status = ENUMS.SurgeStatus.AIMING
+			set_surge_aiming()
 		
-		
-	if event.is_action_pressed("Surge"):
+	if event.is_action_released("Surge"):
 		if surge_status == ENUMS.SurgeStatus.AIMING:
 			fire_surge()
