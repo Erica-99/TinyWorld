bbeca69b005568ddc76af50ac84a4827212a278f
Erica-99 <turbocake17@gmail.com>
2026-02-26 16:07:34 +1100

    Set up turret raycasting

--- scripts/laser_turret.gd
+++ scripts/laser_turret.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
+@onready var aiming_raycast: RayCast2D = $AimingRaycast
+@onready var aiming_line: Line2D = $AimingLaser
+@onready var damage_shapecast: ShapeCast2D = $DamageShapecast
+@onready var damage_line: Line2D = $DamageLaser
+
 var enabled := false
+var target_ship: RigidBody2D = null
+var locked_on = false
+var lock_on_point: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +18,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	if not enabled:
+		return
+	if target_ship != null:
+		if not locked_on:
+			aiming_raycast.target_position = target_ship.global_position - aiming_raycast.global_position
+			if aiming_raycast.is_colliding():
+				var hit_point = aiming_raycast.get_collision_point()
+	pass
+
 
 
 func enable() -> void:
@@ -23,6 +43,15 @@ func disable() -> void:
 	pass
 
 
+func reset_aiming() -> void:
+	target_ship = null
+	pass
+
+
 # Triggers when player enters aggro range
 func _on_aggro_range_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	target_ship = body as RigidBody2D
+
+
+func _on_aggro_range_body_exited(body: Node2D) -> void:
+	reset_aiming()
